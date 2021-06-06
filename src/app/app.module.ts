74690b8a6799e1d3326f7a3ec8f17dcba8df7a01
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-06-06 20:19:02 -0300

    adição da chamada da função de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { ChartComponent } from './chart/chart.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, SigninComponent, ChartComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 const API = 'http://localhost:3333/';
 
@@ -8,26 +9,35 @@ const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
+interface User {
+  user: Object;
+  token: string;
+}
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
-  form: any = {
-    email: null,
-    password: null,
-  };
+  login = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {}
 
-  onSubmit(): void {
-    const { email, password } = this.form;
-    console.log(email, password);
+  async onSubmit(): Promise<void> {
+    console.warn(this.login.value);
 
-    const response = this.http
+    const { email, password } = {
+      email: this.login.value.email,
+      password: this.login.value.password,
+    };
+
+    this.http
       .post(
         API + 'sessions',
         {
@@ -36,11 +46,10 @@ export class SigninComponent implements OnInit {
         },
         httpOptions
       )
-      .subscribe((resp) => {
-        console.log(resp);
+      .subscribe((data) => {
+        const tranformedData = data as User;
+        localStorage.setItem('token', tranformedData.token);
+        this.router.navigateByUrl('/chart');
       });
-
-    console.log(response);
-    this.router.navigateByUrl('/chart');
   }
 }
