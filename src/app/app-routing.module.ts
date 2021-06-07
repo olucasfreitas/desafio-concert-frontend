99f1d6c67a96a9a5a0c08d9ca64cb162104dd618
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-06-06 22:51:12 -0300

    adição da rota de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: SigninComponent },
   { path: 'chart', component: ChartComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: '' },
 ];
 
